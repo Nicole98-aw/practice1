215905572adfa651f3b549419a743c921fa0bf28
Nicole98_aw <nicoleotieno18@gmail.com>
2019-09-12 10:41:50 +0300

    added index paragraph

---
+++ practise1.js
@@ -0,0 +1,4 @@
+var element1 = document.querySelector("h1");
+element1.innerText = "My fav programming language";
+var element2 = document.querySelector("p");
+element2.innerText = "I love to code in JS";
