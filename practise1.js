ff8a934050c7d035b24ab90fa90cb1591ff8b79f
Nicole <nicoleotieno18@gmail.com>
2019-11-07 11:10:45 +0300

    use getters to get internal properties of objects

--- practise1.js
+++ practise1.js
@@ -1,34 +1,27 @@
-let spaceship = {
-    crew: {
-    captain: { 
-        name: 'Lily', 
-        degree: 'Computer Engineering', 
-        cheerTeam() { console.log('You got this!') } 
-        },
-    'chief officer': { 
-        name: 'Dan', 
-        degree: 'Aerospace Engineering', 
-        agree() { console.log('I agree, captain!') } 
-        },
-    medic: { 
-        name: 'Clementine', 
-        degree: 'Physics', 
-        announce() { console.log(`Jets on!`) } },
-    translator: {
-        name: 'Shauna', 
-        degree: 'Conservation Science', 
-        powerFuel() { console.log('The tank is full!') } 
+// const person = {
+//     _firstName:'Nicky',
+//     _lastName: 'Gandolfo',
+//     get fullName() {
+//         if(this._firstName && this._lastName) {
+//             return `${this._firstName} ${this._lastName}`;
+//         } else {
+//             return 'Missing a first name or a last name';
+//         }
+//     }
+// }
+// person.fullName;
+// console.log(person.fullName);
+
+const car = {
+    _name: 'Audi RS6',
+    _color: 'silver',
+    get color() {
+        if (this._color = 'silver') {
+            return `My Audi's color is ${this._color}`;
+        } else {
+            return 'Invalid input';
         }
     }
-}; 
-
-
-for (let crewMember in spaceship.crew) {
-  console.log(`${crewMember}: ${spaceship.crew[crewMember].name}`)
-};
-
-for (let crewMember in spaceship.crew) {
-  console.log(`${spaceship.crew[crewMember].name}: ${spaceship.crew[crewMember].degree}`)
-};
-
+}
+console.log(car.color);
 
